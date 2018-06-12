ebf3366ab074a5ef2c507645ca789619e539a0c6
Robin Allison <robinallison@Robins-MacBook-Pro.local>
2018-06-11 21:02:06 -0700

    so it turns out that you have to do 'git add paint.py' in order to update paint.py. Otherwise when I try 'git push origin master' it says 'no changes added to commit'. Anyway I updated paint.py so that clicking on the screeen returns coordinate data on the command line, but nothing on the screen actually changes.

--- paint.py
+++ paint.py
@@ -1,4 +1,4 @@
-#only gives black screen
+#clicking a point on the screen is a MotionEvent and returns on the command line spos =(x-coord, y-coord) where the lower left is (0,0) and the upper right is (1,1,). It also returns pos=(something, somethingelse), but I don't know what this is.
 import kivy
 
 from kivy.app import App
@@ -6,7 +6,8 @@ from kivy.uix.widget import Widget
 
 
 class MyPaintWidget(Widget):
-    pass
+    def on_touch_down(self, touch):
+        print(touch)
 
 
 class MyPaintApp(App):
