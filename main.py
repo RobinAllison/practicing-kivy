0eb10a8c1d3ea4397b8b3f826fefcd3e8d30c8a3
Robin Allison <robinps2@sbcglobal.net>
2018-06-21 13:22:25 -0700

    main.py and pong.kv still work

--- main.py
+++ main.py
@@ -2,28 +2,19 @@ import kivy
 kivy.require('1.10.0')
 
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 
-class LoginScreen(GridLayout):
 
-	def __init__(self, **kwargs):
-	    super(LoginScreen, self).__init__(**kwargs)
-	    self.cols = 2
-	    self.add_widget(Label(text='User Name'))
-	    self.username = TextInput(multiline=False)
-	    self.add_widget(self.username)
-	    self.add_widget(Label(text='password'))
-	    self.password = TextInput(password=True, multiline=False)
-	    self.add_widget(self.password)
 
-class MyApp(App):
+class PongGame(Widget):
+    pass
 
-	def build(self):
-	    return LoginScreen()
+
+class PongApp(App):
+    def build(self):
+        return PongGame()
 
 
 
 if __name__ == '__main__':
-	MyApp().run()
+	PongApp().run()
